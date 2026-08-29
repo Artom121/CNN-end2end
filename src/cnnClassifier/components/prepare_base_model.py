240a4e4515a5b29e49f8d295ead4dffb08b3941b
Artom121 <artm.vardanyan@list.ru>
2026-08-29 05:51:01 +0300

    model training add

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -55,6 +55,7 @@ class PrepareBaseModel:
             freeze_till=None,
             learning_rate=self.config.params_learning_rate
         )
+        self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
     
     @staticmethod
     def save_model(path: Path, model: tf.keras.Model):
