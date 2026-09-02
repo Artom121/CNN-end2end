0e988739acd40ed1275ce94e8a4f2cc1f65ca9ce
Artom121 <artm.vardanyan@list.ru>
2026-09-02 10:53:13 +0300

    prediction ui added

---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,30 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
+
+
+class PredictionPipeline:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def predict(self):
+        model = load_model(os.path.join('artifacts', 'training', 'model.h5'))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size=(224, 224))
+        test_image = image.img_to_array(test_image)
+        test_image = test_image / 255.0
+        test_image = np.expand_dims(test_image, axis=0)
+        prediction = model.predict(test_image)
+        result = np.argmax(prediction, axis=1)
+        print(result)
+        print("Raw prediction:", prediction)
+
+
+        if result[0] == 1:
+            prediction = 'Tumor'
+            return [{'image': prediction}]
+        else:
+            prediction = 'Normal'
+            return [{'image': prediction}]
